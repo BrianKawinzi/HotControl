45eb6074fe7633b571cf60a775c70676b947a7c2
BrianKawinzi <brian.mutisya@strathmore.edu>
2024-05-24 06:03:59 +0300

    expense and inventory added

--- lib/multi_pages/general_pages/expense_multi.dart
+++ lib/multi_pages/general_pages/expense_multi.dart
@@ -1,10 +1,245 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ExpenseMulti extends StatelessWidget {
+class ExpenseMulti extends StatefulWidget {
   const ExpenseMulti({super.key});
 
   @override
+  State<ExpenseMulti> createState() => _ExpenseMultiState();
+}
+
+class _ExpenseMultiState extends State<ExpenseMulti> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> expenses = [];
+  List<Map<String, dynamic>> departments = [];
+  String? selectedDepartmentId;
+  String? selectedDepartmentName;
+
+  @override
+  void initState() {
+    super.initState();
+    _getDeptDetails();
+  }
+
+  Future<void> _getDeptDetails() async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('departments').get();
+
+      setState(() {
+        departments = snapshot.docs.map((doc) {
+          return {
+            'id': doc.id,
+            'name': doc['name'],
+            'operator': doc['operator'],
+          };
+        }).toList();
+      });
+    }
+  }
+
+  Future<void> _getExpensesForDepartment(String departmentId) async {
+    final QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore
+        .collection('expenses')
+        .where('departmentId', isEqualTo: departmentId)
+        .get();
+
+    setState(() {
+      expenses = snapshot.docs.map((doc) {
+        return {
+          'id': doc.id,
+          'name': doc['name'],
+          'amount': doc['amount'],
+          'departmentId': departmentId,
+        };
+      }).toList();
+    });
+  }
+
+  void _addExpense(String name, double amount) {
+    setState(() {
+      expenses.add({
+        'name': name,
+        'amount': amount,
+        'departmentId': selectedDepartmentId,
+      });
+    });
+  }
+
+  void _saveExpenses() async {
+    for (var expense in expenses) {
+      if (expense.containsKey('id')) {
+        await _firestore.collection('expenses').doc(expense['id']).update(expense);
+      } else {
+        await _firestore.collection('expenses').add(expense);
+      }
+    }
+    Navigator.pop(context);
+  }
+
+  void _deleteExpense(int index) async {
+    if (expenses[index].containsKey('id')) {
+      await _firestore.collection('expenses').doc(expenses[index]['id']).delete();
+    }
+    setState(() {
+      expenses.removeAt(index);
+    });
+  }
+
+  void _editExpense(int index, String name, double amount) {
+    setState(() {
+      expenses[index] = {
+        'id': expenses[index]['id'],
+        'name': name,
+        'amount': amount,
+        'departmentId': selectedDepartmentId,
+      };
+    });
+  }
+
+  // Text controllers
+  TextEditingController nameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+
+  // Show add/edit expense dialog
+  void _showAddExpenseDialog({int? index}) {
+    if (index != null) {
+      nameController.text = expenses[index]['name'];
+      amountController.text = expenses[index]['amount'].toString();
+    } else {
+      nameController.clear();
+      amountController.clear();
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(index == null ? 'Add Expense' : 'Edit Expense'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: const InputDecoration(hintText: 'Amount'),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (nameController.text.isNotEmpty && amountController.text.isNotEmpty) {
+                if (index == null) {
+                  _addExpense(
+                    nameController.text,
+                    double.parse(amountController.text),
+                  );
+                } else {
+                  _editExpense(
+                    index,
+                    nameController.text,
+                    double.parse(amountController.text),
+                  );
+                }
+                nameController.clear();
+                amountController.clear();
+                Navigator.pop(context);
+              }
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _selectDepartment(String departmentId, String departmentName) {
+    setState(() {
+      selectedDepartmentId = departmentId;
+      selectedDepartmentName = departmentName;
+    });
+    _getExpensesForDepartment(departmentId);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: selectedDepartmentId != null
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedDepartmentId = null;
+                    selectedDepartmentName = null;
+                    expenses = [];
+                  });
+                },
+                icon: const Icon(Icons.arrow_back),
+              )
+            : null,
+        title: Text(selectedDepartmentId == null
+            ? 'Departments'
+            : 'Expenses for $selectedDepartmentName'),
+      ),
+      body: selectedDepartmentId == null
+          ? ListView.builder(
+              itemCount: departments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(departments[index]['name']),
+                  subtitle: Text(departments[index]['operator']),
+                  onTap: () => _selectDepartment(
+                    departments[index]['id'],
+                    departments[index]['name'],
+                  ),
+                );
+              },
+            )
+          : ListView.builder(
+              itemCount: expenses.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(expenses[index]['name']),
+                  subtitle: Text('Amount: \$${expenses[index]['amount']}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.settings, color: Colors.blue),
+                        onPressed: () {
+                          _showAddExpenseDialog(index: index);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          _deleteExpense(index);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: selectedDepartmentId != null
+          ? FloatingActionButton(
+              onPressed: () => _showAddExpenseDialog(),
+              child: const Icon(Icons.add),
+            )
+          : null,
+    );
   }
 }
--- lib/multi_pages/general_pages/inventory_multi.dart
+++ lib/multi_pages/general_pages/inventory_multi.dart
@@ -1,10 +1,275 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class InventoryMulti extends StatelessWidget {
+class InventoryMulti extends StatefulWidget {
   const InventoryMulti({super.key});
 
   @override
+  State<InventoryMulti> createState() => _InventoryMultiState();
+}
+
+class _InventoryMultiState extends State<InventoryMulti> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> inventory = [];
+  List<Map<String, dynamic>> departments = [];
+  String? selectedDepartmentId;
+  String? selectedDepartmentName;
+
+  @override
+  void initState() {
+    super.initState();
+    _getDeptDetails();
+  }
+
+  Future<void> _getDeptDetails() async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await _firestore.collection('departments').get();
+
+      setState(() {
+        departments = snapshot.docs.map((doc) {
+          return {
+            'id': doc.id,
+            'name': doc['name'],
+            'operator': doc['operator'],
+          };
+        }).toList();
+      });
+    }
+  }
+
+  Future<void> _getInventoryForDepartment(String departmentId) async {
+    final QuerySnapshot<Map<String, dynamic>> snapshot = await _firestore
+        .collection('inventory')
+        .where('departmentId', isEqualTo: departmentId)
+        .get();
+
+    setState(() {
+      inventory = snapshot.docs.map((doc) {
+        return {
+          'id': doc.id,
+          'name': doc['name'],
+          'bp': doc['bp'],
+          'sp': doc['sp'],
+          'quantities': doc['quantities'],
+        };
+      }).toList();
+    });
+  }
+
+  void _addInventory(String name, double bp, double sp, int quantities) {
+    setState(() {
+      inventory.add({
+        'name': name,
+        'bp': bp,
+        'sp': sp,
+        'quantities': quantities,
+        'departmentId': selectedDepartmentId,
+      });
+    });
+  }
+
+  void _saveInventory() async {
+    for (var item in inventory) {
+      if (item.containsKey('id')) {
+        await _firestore.collection('inventory').doc(item['id']).update(item);
+      } else {
+        await _firestore.collection('inventory').add(item);
+      }
+    }
+    Navigator.pop(context);
+  }
+
+  void _deleteInventory(int index) async {
+    if (inventory[index].containsKey('id')) {
+      await _firestore.collection('inventory').doc(inventory[index]['id']).delete();
+    }
+    setState(() {
+      inventory.removeAt(index);
+    });
+  }
+
+  void _editInventory(int index, String name, double bp, double sp, int quantities) {
+    setState(() {
+      inventory[index] = {
+        'id': inventory[index]['id'],
+        'name': name,
+        'bp': bp,
+        'sp': sp,
+        'quantities': quantities,
+        'departmentId': selectedDepartmentId,
+      };
+    });
+  }
+
+  // Text controllers
+  TextEditingController nameController = TextEditingController();
+  TextEditingController bpController = TextEditingController();
+  TextEditingController spController = TextEditingController();
+  TextEditingController quantitiesController = TextEditingController();
+
+  // Show add/edit inventory dialog
+  void _showAddInventoryDialog({int? index}) {
+    if (index != null) {
+      nameController.text = inventory[index]['name'];
+      bpController.text = inventory[index]['bp'].toString();
+      spController.text = inventory[index]['sp'].toString();
+      quantitiesController.text = inventory[index]['quantities'].toString();
+    } else {
+      nameController.clear();
+      bpController.clear();
+      spController.clear();
+      quantitiesController.clear();
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(index == null ? 'Add Inventory' : 'Edit Inventory'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+            TextField(
+              controller: bpController,
+              decoration: const InputDecoration(hintText: 'Buying Price'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: spController,
+              decoration: const InputDecoration(hintText: 'Selling Price'),
+              keyboardType: TextInputType.number,
+            ),
+            TextField(
+              controller: quantitiesController,
+              decoration: const InputDecoration(hintText: 'Quantities'),
+              keyboardType: TextInputType.number,
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Cancel'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              if (nameController.text.isNotEmpty &&
+                  bpController.text.isNotEmpty &&
+                  spController.text.isNotEmpty &&
+                  quantitiesController.text.isNotEmpty) {
+                if (index == null) {
+                  _addInventory(
+                    nameController.text,
+                    double.parse(bpController.text),
+                    double.parse(spController.text),
+                    int.parse(quantitiesController.text),
+                  );
+                } else {
+                  _editInventory(
+                    index,
+                    nameController.text,
+                    double.parse(bpController.text),
+                    double.parse(spController.text),
+                    int.parse(quantitiesController.text),
+                  );
+                }
+                nameController.clear();
+                bpController.clear();
+                spController.clear();
+                quantitiesController.clear();
+                Navigator.pop(context);
+              }
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _selectDepartment(String departmentId, String departmentName) {
+    setState(() {
+      selectedDepartmentId = departmentId;
+      selectedDepartmentName = departmentName;
+    });
+    _getInventoryForDepartment(departmentId);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: selectedDepartmentId != null
+            ? IconButton(
+                onPressed: () {
+                  setState(() {
+                    selectedDepartmentId = null;
+                    selectedDepartmentName = null;
+                    inventory = [];
+                  });
+                },
+                icon: const Icon(Icons.arrow_back),
+              )
+            : null,
+        title: Text(selectedDepartmentId == null
+            ? 'Departments'
+            : 'Inventory for $selectedDepartmentName'),
+      ),
+      body: selectedDepartmentId == null
+          ? ListView.builder(
+              itemCount: departments.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(departments[index]['name']),
+                  subtitle: Text(departments[index]['operator']),
+                  onTap: () => _selectDepartment(
+                    departments[index]['id'],
+                    departments[index]['name'],
+                  ),
+                );
+              },
+            )
+          : ListView.builder(
+              itemCount: inventory.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(inventory[index]['name']),
+                  subtitle: Text('Quantities: ${inventory[index]['quantities']}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.settings, color: Colors.blue),
+                        onPressed: () {
+                          _showAddInventoryDialog(index: index);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          _deleteInventory(index);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: selectedDepartmentId != null
+          ? FloatingActionButton(
+              onPressed: () => _showAddInventoryDialog(),
+              child: const Icon(Icons.add),
+            )
+          : null,
+    );
   }
 }
